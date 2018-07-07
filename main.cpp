0e4ec32f1351cf78ebb1e6ccbaef7904d9a6784f
maximProggrammer <maxrocers@gmail.com>
2018-07-08 04:26:55 +0600

    Будем пробывать улучшать скорость проработки кода.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,65 @@
 
 using namespace std;
 
+
+bool gameOver;
+const int width = 21;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+
+void Setup(){
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void draw(){
+    system("cls");
+    for(int i = 0; i < width; i++){
+        cout << "#";
+    }
+    cout << endl;
+
+    for(int i = 0; i < height; i++){
+
+        for(int j = 0; j < width; j++){
+            if(j == 0 || j == width - 1){
+                cout << "#";
+            }
+            cout << " ";
+        }
+    }
+    cout << endl;
+
+    for(int i = 0; i < width; i++){
+        cout << "#";
+    }
+    cout << endl;
+}
+
+void Input(){
+
+}
+
+void logic(){
+
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    Setup();
+
+    while (!gameOver){
+        logic();
+        draw();
+        Input();
+    }
+
     return 0;
 }
